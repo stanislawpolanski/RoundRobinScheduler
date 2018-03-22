01fc4d59411c5fe29460f7a19fe0eef1ada52d43
Staszek <Staszek@STASZEK-PC>
2018-03-22 21:29:51 +0100

    Initial commit with Team and District classes.

---
+++ RoundRobinScheduler/District.cs
@@ -0,0 +1,34 @@
+﻿namespace RoundRobinScheduler
+{
+    public class District
+    {
+        /// <summary>
+        /// Disctrict's id.
+        /// </summary>
+        public int Id
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Name of the district
+        /// </summary>
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates a district.
+        /// </summary>
+        /// <param name="id">Id of the district</param>
+        /// <param name="name">Name of the district</param>
+        District(int id, string name)
+        {
+            this.Id   = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ RoundRobinScheduler/Team.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Describes the team using its identification number and name.
+/// </summary>
+namespace RoundRobinScheduler
+{
+    class Team
+    {
+        /// <summary>
+        /// Id of the team.
+        /// </summary>
+        public int Id
+        {
+            private set;
+            get;
+        }
+
+        /// <summary>
+        /// Team's name.
+        /// </summary>
+        public string Name
+        {
+            private set;
+            get;
+        }
+
+        /// <summary>
+        /// Represents the district to which the team is assigned.
+        /// </summary>
+        public District DistrictAssignment
+        {
+            private set;
+            get;
+        }
+
+        /// <summary>
+        /// Creates a team. The team has a name and and identification number.
+        /// </summary>
+        /// <param name="id">Identification number of the team</param>
+        /// <param name="name">Team's name</param>
+        /// <param name="da">Team's district assignment</param>
+        Team(int id, string name, District da)
+        {
+            this.Id   = id;
+            this.Name = name;
+            this.DistrictAssignment = da;
+        }
+    }
+}
