20b8a117242739259eb2c4b52a83ad2f639e6125
Staszek <Staszek@STASZEK-PC>
2018-03-26 13:03:14 +0200

    Some basics of round class

--- RoundRobinScheduler/Round.cs
+++ RoundRobinScheduler/Round.cs
@@ -5,10 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-//TODO to be implemented
+//TODO is there a need to store it as a 
 namespace RoundRobinScheduler
 {
-    interface Round
+    class Round : List<Match>
     {
+        public readonly int OrdinalNumber;
+
+        public Round(int on)
+        {
+            if (on > 0)
+                this.OrdinalNumber = on;
+            else
+                throw new Exception();
+            //TODO some special exception?
+        }
     }
 }
